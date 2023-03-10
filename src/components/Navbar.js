810152af8b80e779147d925bf17c5440ed15bf03
Nguyen Duc Thang <thangngdc03bn@gmail.com>
2023-03-11 01:05:15 +0800

    Add animation for dropdown menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react"
 export default function Navbar(props) {
     const [currentClass, setCurrentClass] = React.useState("")
+    const [openStatus, setOpenStatus] = React.useState("")
     const handleKeyPress = (event) => {
         if(event.key === 'Enter'){
             props.getData(event.target.value)
         }
       }
+
     const handleClickDropdown = () => {
         setCurrentClass(prevClass => { 
             if (prevClass === "" || prevClass === "rotate--back") {
@@ -14,16 +16,35 @@ export default function Navbar(props) {
                 return "rotate--back"
             }
         })
+        setOpenStatus(prevStatus => {
+            if (prevStatus === "" || prevStatus === "close") {
+                return "open"
+            } else {
+                return "close"
+            }
+        })
     }
+
     return (
         <nav>
-            <button className="dropdown">
+            <button className="dropdown" onClick={handleClickDropdown}>
                 <div>Features</div>
-                <img className = {currentClass} src = "images/triangle.png" alt = "" width = {10}px 
-                onClick={handleClickDropdown}></img>
-                </button>
+                <img className = {currentClass} src = "images/triangle.png" alt = "" width = {10}px></img>
+            </button>
+            <div className={`feature--list ${openStatus}`} >
+                <div className="feature--line">
+                    <img className = "feature--image" src = "./images/predict.png"></img><span>Prediction</span>
+                </div>
+                <div className="feature--line">
+                    <img className = "feature--image" src = "./images/bug.png"></img><span>Report bug</span>
+                </div>
+                <div className="feature--line">
+                    <img className = "feature--image" src = "./images/alert.png"></img><span>Alert</span>
+                </div>
+            </div>
             <input type = {"text"} className="search--box" placeholder="Enter your search here" onKeyDown = {handleKeyPress}>
             </input>
         </nav>
     )
+
 }
